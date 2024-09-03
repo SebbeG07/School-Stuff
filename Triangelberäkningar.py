a82f0fc6717966c66c5b87daafc4391bb2d45769
SebbeG07 <sebastianwolff07@gmail.com>
2024-09-03 09:23:34 +0200

    School

---
+++ Triangelberäkningar.py
@@ -0,0 +1,20 @@
+import math
+
+tal2=math.sqrt((3 ** 2) + (4 ** 2))
+print(tal2)
+print(round(tal2, 2))
+
+tal3=math.sqrt((2 ** 2) + (5 ** 2))
+print(tal3)
+print(round(tal3, 2))
+
+tal4=math.sqrt((14 ** 2) + (11 ** 2))
+print(tal4)
+print(round(tal4, 2))
+
+yes1=float(input("Skriv en katet: "))
+yes2=float(input("Skriv en till: "))
+
+talet=math.sqrt((yes1 ** 2) + (yes2 ** 2))
+print(talet)
+print(round(talet, 2))
