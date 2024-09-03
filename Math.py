8848c2c19bee5b937fa1431eb995c280233ecfa3
SebbeG07 <sebastianwolff07@gmail.com>
2024-09-03 08:45:01 +0200

    School

---
+++ Math.py
@@ -0,0 +1,11 @@
+import math
+
+tal1=((3 + 145) * 7 )
+
+tal2=(7 ** 7)
+
+tal3=((2 + 3) * (2 ** 2))
+
+tal4=((3 ** 3) * (4 ** 4))
+
+print("Tal1: ",tal1,"\nTal2: ", tal2,"\nTal3: ",tal3, "\nTal4: ",tal4)
