98a7b2b99648bb8fdf1de0d47b4d6ea78aefa5f8
SebbeG07 <sebastianwolff07@gmail.com>
2024-09-05 09:03:10 +0200

    School

---
+++ Omvandling grader.py
@@ -0,0 +1,23 @@
+import math 
+
+thing1=math.radians(160)
+print (thing1)
+
+thing2=math.radians(360)
+print (thing2)
+
+thing3=math.radians(90)
+print (thing3)
+
+rad1=float(input("Ange Ett Gradantal: "))
+thing4=math.radians(rad1)
+print (thing4)
+
+rad2=float(input("Ange Ett Gradantal: "))
+thing5=math.radians(rad2)
+
+
+yes1=int(input("Antal decimaler?: "))
+talet=thing5
+print(talet)
+print(round(talet, yes1))
