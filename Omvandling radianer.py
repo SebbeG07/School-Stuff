75d6be2407326178f28d588c69b6f7ccda23cd9e
SebbeG07 <sebastianwolff07@gmail.com>
2024-09-12 08:14:25 +0200

    School

---
+++ Omvandling radianer.py
@@ -0,0 +1,23 @@
+import math
+
+Thing1=math.degrees(math.pi)
+print (Thing1)
+
+Thing2=math.degrees(math.pi*2)
+print (Thing2)
+
+Thing3=math.degrees(math.pi/2)
+print (Thing3)
+
+rad1=float(input("Ange något med pi: "))
+Thing4=math.degrees(math.pi*rad1)
+print (Thing4)
+
+rad2=float(input("Ange något med pi: "))
+Thing5=math.degrees(math.pi*rad2)
+print (Thing5)
+
+yes1=int(input("Antal decimaler?: "))
+talet=Thing5
+print(talet)
+print(round(talet, yes1))
