7c6401d1927b177d8a6d5d8da2ceba6ae50a861c
SebbeG07 <sebastianwolff07@gmail.com>
2024-10-03 09:05:47 +0200

    School

---
+++ Fiskmås, Bläckfisk eller Bäver.py
@@ -0,0 +1,13 @@
+tal1=int(input ("Ange ett tal: "))
+
+if tal1<0:
+    print ("Ditt tal är negativt")
+
+elif tal1==0:
+    print ("Ditt tal är 0")
+
+elif tal1>0:
+    print ("Ditt tal är posetivt")
+
+else:
+    print ("Hur lyckas du att skriva fel?")
